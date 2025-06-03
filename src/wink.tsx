e8b9f31a8fbf3520d652576356cc76bdea02e6d8
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-06-03 22:51:19 +0200

    winkHtmlAdd

---
+++ src/wink.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './wink.tsx'
+import './wink.css'
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
