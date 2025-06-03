bce26a87a766342e0b28297556400850cd6491b3
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-06-03 23:21:27 +0200

    maj

--- src/wink.tsx
+++ src/wink.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import wink from './wink.tsx'
 import './wink.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('wink')!).render(
     <StrictMode>
         <wink />
     </StrictMode>,
